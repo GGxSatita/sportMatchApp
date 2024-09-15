196b115434a9e0ac4ee0a157a4ff69d2314fc369
Iván Carreño <105950912+GGxSatita@users.noreply.github.com>
2024-09-14 21:25:55 -0300

    "se agrega la app sportMatch"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Imports necesarios de firebase
+import { initializeApp } from "firebase/app";
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { initializeApp as initializeApp_alias, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [AppComponent, ],
+  imports: [
+      BrowserModule,
+      IonicModule.forRoot(), 
+      AppRoutingModule,
+      ReactiveFormsModule,
+      AngularFireModule.initializeApp(environment.firebaseConfig)
+    ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"sportmach-fc07f","appId":"1:276809292205:web:fcef7335a06702d4496924","storageBucket":"sportmach-fc07f.appspot.com","apiKey":"AIzaSyBYGCbdr-FSDMtB_-PvDKf7OIJuo1-a5nE","authDomain":"sportmach-fc07f.firebaseapp.com","messagingSenderId":"276809292205"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
